d67359b056c0e357350d50277acc0e63f4b428a6
laura <lauramlissa@gmail.com>
2018-07-21 18:11:46 +0100

    Add css file and slug field

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    topic = models.CharField(max_length=200, blank=True, null=True)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
         default = timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +19,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
