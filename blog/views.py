05e7387a968987f2a5529e77e35938e5e8c0428f
laura <lauramlissa@gmail.com>
2018-07-30 17:17:03 +0100

    Add logo to home page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from django.shortcuts import render
 from django.utils import timezone
@@ -8,7 +9,11 @@ from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    page = request.GET.get('page',1)
+
+    paginator = Paginator(posts_list, 3)
+    posts=paginator.page(page)
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, slug):
