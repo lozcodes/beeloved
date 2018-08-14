6da6dbe110303a20ec727197a5e2fad487f306dd
laura <lauramlissa@gmail.com>
2018-08-14 17:49:20 +0100

    Add droplet IP

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't*ho6vkbep=2=i8(tdt+1k#j97m4=se6u*r1shz4c*d(58n+n7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.beeloved.ie']
+ALLOWED_HOSTS = ['209.97.137.94', 'www.beeloved.ie']
 
 
 # Application definition
